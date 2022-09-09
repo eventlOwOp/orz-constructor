f8acb7c1122babaf3755644d61faa8d77b614de7
bossbaby <3064273390@qq.com>
2022-09-10 00:13:38 +0800

    114514

--- api/index.js
+++ api/index.js
@@ -4,7 +4,6 @@ const path = require("path");
 module.exports = async (req, res) => {
 	const fp0 = path.join(process.cwd(), "api", "index.html");
 	const x = String(await fs.readFile(fp0));
-	console.log(x);
 	res.setHeader("content-type", "text/html; charset=utf-8");
 	res.status(200).send(x);
 };
