6718b28a9b889cb42bc362b8208e3556c4f2d3c6
bossbaby <3064273390@qq.com>
2022-09-11 15:39:53 +0800

    add debug info

--- api/cons.js
+++ api/cons.js
@@ -7,30 +7,26 @@ module.exports = async (req, res) => {
 
 	if (type !== "png" && type !== "gif") res.status(400);
 
+	transparent = parseInt(transparent);
+
 	res.setHeader("Content-Type", `image/${type}`);
 	res.setHeader("Content-Disposition", `attachment; filename=orz.gif`);
 
 	console.log({
+		text,
+		size,
 		pattern: `${pattern}.png`,
 		type,
 		transparent,
 		background,
 	});
 
-	// return res.send(
-	// 	await generate(text, size, {
-	// 		pattern: `${pattern}.png`,
-	// 		type,
-	// 		transparent,
-	// 		background,
-	// 	})
-	// );
 	return res.send(
-		await generate("我太菜了", 16, {
-			type: "gif",
-			transparent: false,
-			background: "#ffffff",
-			pattern: "0.png",
+		await generate(text, size, {
+			pattern: `${pattern}.png`,
+			type,
+			transparent,
+			background,
 		})
 	);
 };
