4de844038a37bada4ac4247e6f01279a3c14c587
bossbaby <3064273390@qq.com>
2022-09-11 11:53:46 +0800

    ld path

--- api/cons.js
+++ api/cons.js
@@ -1,13 +1,6 @@
-if (
-	process.env.LD_LIBRARY_PATH == null ||
-	!process.env.LD_LIBRARY_PATH.includes(
-		`${process.env.PWD}/node_modules/canvas/build/Release:`
-	)
-) {
-	process.env.LD_LIBRARY_PATH = `${
-		process.env.PWD
-	}/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
-}
+process.env.LD_LIBRARY_PATH = `${
+	process.env.PWD
+}/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
 
 const fs = require("fs").promises;
 const GIFEncoder = require("gifencoder");
