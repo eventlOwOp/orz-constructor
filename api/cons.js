7b7dd4970be5e7b214e7cc2382e4957beac107be
bossbaby <3064273390@qq.com>
2022-09-11 15:25:48 +0800

    update multiple file type and multiple choice

--- api/cons.js
+++ api/cons.js
@@ -1,10 +1,21 @@
-const { constructImg } = require("../lib/construct");
+const { generate } = require("../lib/construct");
 
 module.exports = async (req, res) => {
-	const { text, size } = req.query;
+	const { text, size, pattern, type, transparent, background } = req.query;
 
-	res.setHeader("Content-Type", `image/gif`);
+	if (!text) return res.status(400);
+
+	if (type !== "png" && type !== "gif") res.status(400);
+
+	res.setHeader("Content-Type", `image/${type}`);
 	res.setHeader("Content-Disposition", `attachment; filename=orz.gif`);
 
-	return res.send(await constructImg(text, size));
+	return res.send(
+		await generate(text, size, {
+			pattern: `${pattern}.png`,
+			type,
+			transparent,
+			background,
+		})
+	);
 };
--- test/test.js
+++ test/test.js
@@ -1,5 +1,19 @@
-const { constructImg } = require("../lib/construct");
+const { generate } = require("../lib/construct");
+const fs = require("fs/promises");
 
 (async () => {
-	fs.writeFile("1.gif", await constructImg("我太菜了", 16));
+	fs.writeFile(
+		"test/test.gif",
+		await generate("我太菜了", 16, {
+			type: "gif",
+		})
+	);
+	fs.writeFile(
+		"test/test.png",
+		await generate("我太菜了", 16, {
+			type: "png",
+			transparent: true,
+			pattern: "1.png",
+		})
+	);
 })();
