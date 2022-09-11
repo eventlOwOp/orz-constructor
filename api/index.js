c321e6784f105cc738076801ea6e10dc93b18de6
bossbaby <3064273390@qq.com>
2022-09-11 11:42:33 +0800

    serve static file

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs").promises;
-const path = require("path");
-
-module.exports = async (req, res) => {
-	const fp0 = path.join(process.cwd(), "api", "index.html");
-	const x = String(await fs.readFile(fp0));
-	res.setHeader("content-type", "text/html; charset=utf-8");
-	res.status(200).send(x);
-};
